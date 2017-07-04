6d5d481a0670d1848d686de9eab7e4baa943a994
Akash Gupta <akashgup@adobe.com>
2017-07-04 15:37:14 +0530

    removed src1

--- src1/main/java/com/adobe/prj/dao/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.adobe.prj.dao;
-
-import java.util.List;
-
-import com.adobe.prj.entity.Customer;
-
-public interface CustomerDao {
-	List<Customer> getCustomer();
-}
--- src1/main/java/com/adobe/prj/dao/RentalDao.java
+++
@@ -1,7 +0,0 @@
-package com.adobe.prj.dao;
-
-import com.adobe.prj.entity.Rental;
-
-public interface RentalDao {
-	void rent(Rental rental);
-}
