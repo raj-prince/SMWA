90e4ccf3cc91ce2c42bc1ff24a87bb9072b9ec12
Prince Raj Kumar <prinkuma@adobe.com>
2017-07-05 17:40:17 +0530

    hasRoll changes to hasAuthority

--- src/main/java/com/adobe/prj/security/SecurityConfiguration.java
+++ src/main/java/com/adobe/prj/security/SecurityConfiguration.java
@@ -20,27 +20,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
 			throws Exception {
-		/*auth.inMemoryAuthentication().withUser("hello").password("world")
-				.roles("USER", "ADMIN");*/
+			auth.inMemoryAuthentication().withUser("d").password("world")
+				.roles("USER", "ADMIN");
 		
 		auth.jdbcAuthentication().dataSource(dataSource)
 		  .usersByUsernameQuery(
 		   "select user_name,password,1 from user where user_name=?")
 		  .authoritiesByUsernameQuery(
-		   "select user_name, user_type from user where user_name=?");
+		   "select user_name,user_type from user where user_name=?");
 		 } 
 	
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-<<<<<<< HEAD
-		http.authorizeRequests().antMatchers("/login","/").permitAll()
-				.antMatchers("/").access("hasRole('0')").and()
-=======
+
 		http.authorizeRequests().antMatchers("/login").permitAll()
-			.antMatchers("/").access("hasRole('0')").and()
-				/*.antMatchers("/*.do/**").access("hasRole('1')").and()*/
->>>>>>> a7d34299747991e6ce79dbd836a8860a2b1cccf0
+			.antMatchers("/").access("hasAuthority('0')").and()
 				.formLogin().and().exceptionHandling()
 				.accessDeniedPage("/access-denied");
 	}
