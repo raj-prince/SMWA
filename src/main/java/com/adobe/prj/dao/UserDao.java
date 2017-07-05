721e69e4687095ab406dfde081a07b5f2e5ef274
Prince Raj Kumar <prinkuma@adobe.com>
2017-07-05 15:24:05 +0530

    SpringSecurity working

--- src/main/java/com/adobe/prj/dao/UserDao.java
+++ src/main/java/com/adobe/prj/dao/UserDao.java
@@ -1,6 +1,5 @@
 package com.adobe.prj.dao;
 
-<<<<<<< HEAD
 import java.util.List;
 
 import com.adobe.prj.entity.User;
@@ -10,10 +9,4 @@ public interface UserDao {
 	int addUser(User user);
 	User getUser(int userId);
 	void updateUser(User user);
-=======
-import com.adobe.prj.entity.User;
-
-public interface UserDao {
-	int addUser(User user);
->>>>>>> 6d5d481a0670d1848d686de9eab7e4baa943a994
 }
